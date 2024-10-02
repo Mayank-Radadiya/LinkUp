b9285e921192307929d260f768b5c15eca257163
Mayank-Radadiya <mayankradadiya2@gmail.com>
2024-10-02 22:21:10 +0530

    -

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/HomePage/Home";
-import Login from "./Pages/LoginPage/Login";
 import Register from "./Pages/LoginPage/Register";
 import Profile from "./Pages/ProfilePage/Profile";
 import { useSelector } from "react-redux";
@@ -10,6 +9,8 @@ import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme.js";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Navbar from "./Pages/Navbar/Navbar.jsx";
+import LoginPage from "./Pages/LoginPage/LoginPage.jsx";
+
 
 function App() {
   const mode  = useSelector( (state) => state.mode)
@@ -23,7 +24,7 @@ function App() {
           <Routes>
           <Route path="/" element={<Navbar />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<LoginPage/>} />
           <Route path="/register" element={<Register />} />
           <Route path="/profile/:userId" element={<Profile />} />
           </Routes>
